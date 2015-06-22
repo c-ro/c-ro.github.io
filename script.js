23e4f8d9ed94691dd9f4a8b1a96f9720878c167b
c-ro <carlryanphoto@gmail.com>
2015-06-22 13:48:36 -0400

    linkage problems

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-var bgcolorlist=new Array("#D3EBE6", "#DAD3EB", "#EBD3D8", "#F8FFE6", "#E6FAFF","#F8E6FF", "#FFECE6");
+$(document).ready(function(){
+	paintBG();
+	displayLinks();
+});
 
-thecolor = bgcolorlist[Math.floor(Math.random()*bgcolorlist.length)];
-document.body.style.background = thecolor;
+function paintBG() {
+	var bgcolorlist=new Array("#D3EBE6", "#DAD3EB", "#EBD3D8", "#F8FFE6", "#E6FAFF","#F8E6FF", "#FFECE6");
 
-var urls = new Array("https://www.twitter.com/gnirtsmodnar", "https://github.com/c-ro", "http://carltography.tumblr.com");
+	var thecolor = bgcolorlist[Math.floor(Math.random()*bgcolorlist.length)];
+	document.body.style.background = thecolor;
+}
 
-for (i = 0; i < urls.length; i++){
-    var a = $('<a>');
-    var linktext = urls[i].replace(/https:\/\/|http:\/\/|/,'');
-    linktext = linktext.replace(/www.twitter.com\//,'@');
-    a.attr("href", urls[i]);
-    a.html(linktext);
-    $(a).appendTo('#main');
-};
+function displayLinks() {
+	var urls = new Array("https://www.twitter.com/gnirtsmodnar", "https://github.com/c-ro", "http://carltography.tumblr.com");
+
+	for (i = 0; i < urls.length; i++){
+	    var a = $('<a>');
+	    var linktext = urls[i].replace(/https:\/\/|http:\/\/|/,'');
+	    linktext = linktext.replace(/www.twitter.com\//,'@');
+	    a.attr("href", urls[i]);
+	    a.html(linktext);
+	    $(a).appendTo('#main');
+	};
+}
+
+
