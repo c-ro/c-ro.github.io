3ef792b95c42cd15ce298bb26774b9ffad5250a4
c-ro <carlryanphoto@gmail.com>
2015-07-04 12:26:28 -0400

    fixed scope issues, reduced DOM calls

--- whackamole/game.js
+++ whackamole/game.js
@@ -1,9 +1,12 @@
 var moles = [],
-    counter = 0
+    imgCounter = 0
     totalMoles = 0,
     score = 0,
     hole = "slug.png",
     mole = "bush.jpg",
+    cache = [],
+    display = document.getElementById('display');
+    main = document.getElementById('main');
     images = ['trump.png','rubio.png','perry.png','paul.png','pataki.png','jindal.png','huckabee.png','graham.png','fiorina.png','cruz.png','christie.png','carson.png','bush.png'];
 
 var pW = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
@@ -26,19 +29,19 @@ var pieces = Math.floor(viewWidth/80) * Math.floor(viewHeight/80);
 var createMole = function(){
     var mole = document.createElement("img");
     mole.src = hole;
-    mole.id = counter;
+    mole.id = imgCounter;
     mole.width = 80;
     mole.height = 80;
     mole.vis = false;
     moles.push([mole.id, mole.vis]);
     mole.onclick = function(){whack(mole)};
-
-    document.getElementById('main').appendChild(mole);
+    cache.push(mole);
+    main.appendChild(mole);
 }
 
 for(i = 0; i < Math.floor(pieces); i++){
     createMole();
-    counter++;
+    imgCounter++;
 }
 
 function randomHead(){
@@ -60,38 +63,38 @@ function hide(img){
     var img = img.id || img;
     moles[img][1] = false;
     setTimeout(10);
-    document.getElementById(img).setAttribute("src", hole);
+    cache[img].setAttribute("src", hole);
 }
 
 function show(img){
     totalMoles++;
     moles[img][1] = true;
-    document.getElementById('display').innerHTML = totalMoles + "/" + score;
-    var change = document.getElementById(img);
+    display.innerHTML = totalMoles + "/" + score;
     var piece = randomHead();
-    change.setAttribute("src", piece);
+    cache[img].setAttribute("src", piece);
     setTimeout(function(){hide(img)}, Math.random() * (7000 - 1000) + 1000);
 }
 
 function whack(img){
+    var smackFunc = smack;
+    var redFunc = red;
     if (moles[img.id][1]){
-        red(img);
-        smack();
+        redFunc(img);
+        smackFunc();
         moles[img.id][1] = false;
         score++;
         setTimeout(function(){hide(img.id)},125);
-        document.getElementById('display').innerHTML = totalMoles + "/" + score;
+        display.innerHTML = totalMoles + "/" + score;
     }
 
 }
 
 //return random even or odd number based on request
 function random(type){
-    num = Math.floor(Math.random() * pieces);
+    num = Math.floor(Math.random() * pieces - 1);
     return type === "even" && num % 2 === 0 ? num : num + 1;
 }
 
 /// Start the game and set speeds
-
 window.setInterval(function(){show(moles[random("odd")][0])}, (1000 - 500) + 500);
 window.setInterval(function(){show(moles[random("even")][0])}, (1250 - 750) + 750);
