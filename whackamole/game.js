074a73f82d89a9efb3d0f0e22391846a0cd60be9
c-ro <carlryanphoto@gmail.com>
2015-07-04 14:55:10 -0400

    added grid checkout system

--- whackamole/game.js
+++ whackamole/game.js
@@ -1,16 +1,17 @@
-var moles = [],
+var doc = document,
+    moles = [],
     imgCounter = 0
     totalMoles = 0,
     score = 0,
     hole = "slug.png",
     mole = "bush.jpg",
     cache = [],
-    display = document.getElementById('display');
-    main = document.getElementById('main');
+    display = doc.getElementById('display');
+    main = doc.getElementById('main');
     images = ['trump.png','rubio.png','perry.png','paul.png','pataki.png','jindal.png','huckabee.png','graham.png','fiorina.png','cruz.png','christie.png','carson.png','bush.png'];
 
-var pW = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-var pH = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+var pW = Math.max(doc.documentElement.clientWidth, window.innerWidth || 0);
+var pH = Math.max(doc.documentElement.clientHeight, window.innerHeight || 0);
 
 if (pW < pH){
     var viewWidth = pW * 0.95;
@@ -19,22 +20,24 @@ if (pW < pH){
     var viewHeight = pH * 0.55;
     var viewWidth = viewHeight * 1.33;
 }
+        doc.getElementById('container').style.width = viewWidth;
+        doc.getElementById('container').style.height = viewHeight;
+        doc.getElementById('display').style.width = viewWidth;
+        doc.getElementById('display').style.height = viewHeight * 0.05;
 
 var pieces = Math.floor(viewWidth/80) * Math.floor(viewHeight/80);
-        document.getElementById('container').style.width = viewWidth;
-        document.getElementById('container').style.height = viewHeight;
-        document.getElementById('display').style.width = viewWidth;
-        document.getElementById('display').style.height = viewHeight * 0.05;
+
 
 var createMole = function(){
-    var mole = document.createElement("img");
+    var mole = doc.createElement("img");
     mole.src = hole;
     mole.id = imgCounter;
     mole.width = 80;
     mole.height = 80;
     mole.vis = false;
     moles.push([mole.id, mole.vis]);
-    mole.onclick = function(){whack(mole)};
+    whackFunc = whack;
+    mole.onclick = function(){whackFunc(mole)};
     cache.push(mole);
     main.appendChild(mole);
 }
@@ -51,7 +54,7 @@ function randomHead(){
 function red(img){ 
     var filename = img.src.search(/[^\/]*$/);
     filename = img.src.slice(filename);
-    document.getElementById(img.id).setAttribute("src", "hit" + filename);
+    doc.getElementById(img.id).setAttribute("src", "hit" + filename);
 }
 
 function smack() {
@@ -86,15 +89,25 @@ function whack(img){
         setTimeout(function(){hide(img.id)},125);
         display.innerHTML = totalMoles + "/" + score;
     }
-
 }
 
 //return random even or odd number based on request
-function random(type){
-    num = Math.floor(Math.random() * pieces - 1);
-    return type === "even" && num % 2 === 0 ? num : num + 1;
+function random(){ 
+    var num = Math.floor(Math.random() * pieces);
+    return num;
 }
 
 /// Start the game and set speeds
-window.setInterval(function(){show(moles[random("odd")][0])}, (1000 - 500) + 500);
-window.setInterval(function(){show(moles[random("even")][0])}, (1250 - 750) + 750);
+window.setInterval(function(){
+    var num = random();
+    if(moles[num][1] === false){
+        show(moles[num][0]);
+    };
+}, (1500 - 1000) + 1000);
+
+window.setInterval(function(){
+    var num = random();
+    if(moles[num][1] === false){
+        show(moles[num][0]);
+    };
+}, (1250 - 750) + 750);
