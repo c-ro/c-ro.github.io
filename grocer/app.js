f3cb948a26db458b5c48073c778a74b2a889e5ff
c-ro <carlryanphoto@gmail.com>
2015-10-11 11:19:29 -0400

    prefilled list 10/11

--- grocer/app.js
+++ grocer/app.js
@@ -3,18 +3,34 @@ var app = angular.module('grocer', []);
 app.factory('items', [function(){
 	var list = {
 		items: [
-		  { name: 'broccoli', price: 0, qty: 0, units: 'ct', category: "produce"},
-  		  { name: 'lentils', price: 0, qty: 0, units: 'ct', category: "dryandcanned"},
-		  { name: 'beef', price: 0, qty: 0, units: 'lb', category:"meat"},
-		  { name: 'bread', price: 0, qty: 0, units: 'ct', category:"bakery"}, 
-		  { name: 'sauce', price: 0, qty: 0, units: 'oz', category: "dryandcanned"},
-		  { name: 'cheese', price: 0, qty: 0, units: 'lb', category: "dairy"},
-		  { name: 'chives', price: 0, qty: 0, units: 'ct', category: "produce"},
-  		  { name: 'sardines', price: 0, qty: 0, units: 'ct', category: "dryandcanned"},
-		  { name: 'red snapper', price: 0, qty: 0, units: 'lb', category: "meat"},
-		  { name: 'donuts', price: 0, qty: 0, units: 'ct', category: "bakery"}, 
-		  { name: 'rice', price: 0, qty: 0, units: '', category: "dryandcanned"},
-		  { name: 'yogurt', price: 0, qty: 0, units: '', category: "dairy"}
+		  { name: 'honey', price: 0, qty: 0, units: 'lbs', category: "dryandcanned"},
+		  { name: 'miso', price: 0, qty: 0, units: '', category: "dryandcanned"},
+		  { name: 'millet', price: 0, qty: 0, units: '', category: "dryandcanned"},
+		  { name: 'thyme', price: 0, qty: 0, units: '', category: "spices"},
+		  { name: 'rice vinegar', price: 0, qty: 0, units: '', category: "dryandcanned"},
+		  { name: 'fish', price: 0, qty: 0, units: '', category: "meat"},
+		  { name: 'paper towels', price: 0, qty: 0, units: '', category: "household"},
+		  { name: 'lentils', price: 1.69, qty: 2, units: 'lbs', category: "dryandcanned"},
+		  { name: 'lysol toilet cleaner', price: 0, qty: 0, units: '', category: "household"},
+		  { name: 'spray oil', price: 0, qty: 0, units: '', category: "dryandcanned"},
+		  { name: 'scrubber sponges', price: 0, qty: 0, units: '', category: "household"},
+		  { name: 'frozen peas', price: 0, qty: 0, units: '', category: "frozen"},
+		  { name: 'mukimame', price: 0, qty: 0, units: '', category: "frozen"},
+		  { name: 'carrots', price: 0, qty: 0, units: '', category: "produce"},
+		  { name: 'red onion', price: 2.19, qty: 1, units: 'ct', category: "produce"},
+		  { name: 'sanbal oelek', price: 0, qty: 0, units: '', category: "dryandcanned"},
+		  { name: 'half and half', price: 1.29, qty: 1, units: 'ct', category: "dairy"},
+		  { name: 'bathroom cleaner', price: 0, qty: 0, units: '', category: "household"},
+  		  { name: 'hairspray', price: 0, qty: 0, units: '', category: "personal"},
+		  { name: 'razor handle', price: 0, qty: 0, units: '', category: "personal"},
+  		  { name: 'cilantro', price: 0.99, qty: 0, units: 'ct', category: "produce"},
+		  { name: 'shrimp', price: 11.98, qty: 2, units: 'lbs', category: "meat"},
+  		  { name: 'peppers', price: 1, qty: 4, units: '', category: "produce"},
+		  { name: 'corn starch', price: 0, qty: 0, units: '', category: "dryandcanned"},
+  		  { name: 'cous cous', price: 0, qty: 0, units: '', category: "dryandcanned"},
+		  { name: 'mozzarella', price: 0, qty: 0, units: '', category: "dairy"},
+  		  { name: 'capers', price: 0, qty: 0, units: '', category: "dryandcanned"},
+  		  { name: 'chicken', price: 2.49, qty: 2.1, units: 'lbs', category: "meat"}
 		]
 	};
 
@@ -142,3 +158,17 @@ app.controller('MainCtrl', [
 		}
 	}
 ]);
+
+
+		  // { name: 'broccoli', price: 0, qty: 0, units: 'ct', category: "produce"},
+  		//   { name: 'lentils', price: 0, qty: 0, units: 'ct', category: "dryandcanned"},
+		  // { name: 'beef', price: 0, qty: 0, units: 'lb', category:"meat"},
+		  // { name: 'bread', price: 0, qty: 0, units: 'ct', category:"bakery"}, 
+		  // { name: 'sauce', price: 0, qty: 0, units: 'oz', category: "dryandcanned"},
+		  // { name: 'cheese', price: 0, qty: 0, units: 'lb', category: "dairy"},
+		  // { name: 'chives', price: 0, qty: 0, units: 'ct', category: "produce"},
+  		//   { name: 'sardines', price: 0, qty: 0, units: 'ct', category: "dryandcanned"},
+		  // { name: 'red snapper', price: 0, qty: 0, units: 'lb', category: "meat"},
+		  // { name: 'donuts', price: 0, qty: 0, units: 'ct', category: "bakery"}, 
+		  // { name: 'rice', price: 0, qty: 0, units: '', category: "dryandcanned"},
+		  // { name: 'yogurt', price: 0, qty: 0, units: '', category: "dairy"},
