9a0a56f5dfb7cf27d56b7fcc850065df282763f3
c-ro <carlryanphoto@gmail.com>
2016-03-10 12:31:26 -0500

    alert

--- resume-view/sections.js
+++ resume-view/sections.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
 	    var content = $(this).find('.section-content');
 	   
 	    $(this).find('.section-header').click(function(){
+	    	alert('clicked');
 		    $(this).toggleClass('open');
 		    content.slideToggle();
 		}); 
