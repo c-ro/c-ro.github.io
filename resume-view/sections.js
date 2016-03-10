67ceaac5089a7b81a88143365be0c2949e513cbe
c-ro <carlryanphoto@gmail.com>
2016-03-10 12:47:50 -0500

    don't even know what to do now.

--- resume-view/sections.js
+++ resume-view/sections.js
@@ -1,16 +1,20 @@
 $(document).ready(function() {
 
-	$('.resume-section').each(function(){
-	    
-	    var content = $(this).find('.section-content');
-	   	var header = $(this).find('.section-header');
-
-	    header.click(function(){
-	    	alert('clicked');
-		    $(this).toggleClass('open');
-		    content.slideToggle();
-		}); 
-
+	$('.resume-section').click(function(){
+		alert("wtf?");
 	});
+
+	// $('.resume-section').each(function(){
+	    
+	//     var content = $(this).find('.section-content');
+	//    	var header = $(this).find('.section-header');
+
+	//     header.click(function(){
+	//     	alert('clicked');
+	// 	    $(this).toggleClass('open');
+	// 	    content.slideToggle();
+	// 	}); 
+
+	// });
 
 });
