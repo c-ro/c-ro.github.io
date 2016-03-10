f8456b4eedeffa4b89b3378a1b02acb562d48c50
c-ro <carlryanphoto@gmail.com>
2016-03-10 13:41:54 -0500

    :(

--- resume-view/sections.js
+++ resume-view/sections.js
@@ -1,18 +1,21 @@
 $(function(){
 
-   	$('.resume-section').each(function(index, value){
+ //   	$('.resume-section').each(function(){
 
-   		console.log($(this));
+	//     var content = $(this).find('.section-content');
+	//    	var header = $(this).find('.section-header');
 
-	    var content = $(this).find('.section-content');
-	   	var header = $(this).find('.section-header');
+	//     header.click(function(){
+	//     	console.log("works?");
+	// 	    $(this).toggleClass('open');
+	// 	    content.slideToggle();
+	// 	}); 
 
-	    header.click(function(){
-	    	console.log("works?");
-		    $(this).toggleClass('open');
-		    content.slideToggle();
-		}); 
+	// });
 
-	});
+   	$('.resume-section').click(function(){
+   		alert("why the fuck?");
+   	});
+
 
 });
