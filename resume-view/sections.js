114032ce08a0232673c525002a6e42e97fde7719
c-ro <carlryanphoto@gmail.com>
2016-03-10 13:31:43 -0500

    changed jquery location

--- resume-view/sections.js
+++ resume-view/sections.js
@@ -1,8 +1,8 @@
 $(function(){
 
-	$.each([ 52, 97 ], function( index, value ) {
-		  alert( index + ": " + value );
-		});
+	$('div').each(function (index, value){
+	  console.log($(this).attr('href'));
+	});
 
    	$('.resume-section').each(function(){
 
