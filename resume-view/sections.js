3cbf81e49b6d285c82c8b3af7b8d2581d63cc862
c-ro <carlryanphoto@gmail.com>
2016-03-10 12:34:53 -0500

    .each refactor

--- resume-view/sections.js
+++ resume-view/sections.js
@@ -3,8 +3,9 @@ $(document).ready(function() {
 	$('.resume-section').each(function(){
 	    
 	    var content = $(this).find('.section-content');
-	   
-	    $(this).find('.section-header').click(function(){
+	   	var header = $(this).find('.section-header');
+
+	    header.click(function(){
 	    	alert('clicked');
 		    $(this).toggleClass('open');
 		    content.slideToggle();
